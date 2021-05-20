4517a1388c76d73b2f2ca71ff9d23542b6a1d5ab
Ismael-Oliv <ismaelalves1@outlook.com.br>
2021-05-20 15:52:09 -0300

    Second chapter - Debundando a Aplicaçao

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => response.json({ message: 'Esta funcionando' }));
+app.get('/courses', (request, response) => {
+	const { name } = request.body;
+	console.log(name);
+	return response.json({ message: 'ok' });
+});
 
 app.listen(3333, () => console.log('Servidor está rondando'));
